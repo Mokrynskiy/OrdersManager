9713914e9d0038c94335f6d86db0d5d4f7df8db0
mokrynskiy@gmail.com <mokri@DESKTOP-81JJ9U9>
2021-03-17 15:04:59 +0700

    Добавлена модель главного окна и локатор

---
+++ OrdersManager.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersManager.WPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Главное окно";
+
+        public string Title
+        {
+            get => _title; set => Set(ref _title, value);            
+        }
+
+    }
+}
