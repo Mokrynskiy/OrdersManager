a925a15987174d1656ca9601e181114355fc39ef
mokrynskiy@gmail.com <mokri@DESKTOP-81JJ9U9>
2021-03-18 09:28:51 +0700

    Представления и модели Employees, Departments

---
+++ OrdersManager.WPF/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+using MathCore.WPF.ViewModels;
+using OrdersManager.DAL.Entityes;
+using OrdersManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersManager.WPF.ViewModels
+{
+    public class EmployeesViewModel : ViewModel
+    {
+        private readonly IRepository<Employee> employeeRepository;
+
+        public EmployeesViewModel(IRepository<Employee> employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+    }
+}
--- OrdersManager.WPF/ViewModels/MainWindowViewModel.cs
+++ OrdersManager.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MathCore.WPF.ViewModels;
+﻿using MathCore.WPF.Commands;
+using MathCore.WPF.ViewModels;
 using OrdersManager.DAL.Entityes;
 using OrdersManager.Interfaces;
 using OrdersManager.WPF.Services.interfaces;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OrdersManager.WPF.ViewModels
 {
@@ -15,11 +17,23 @@ namespace OrdersManager.WPF.ViewModels
         private string _title = "Главное окно";        
         private readonly IRepository<Employee> _employeeRepository;
         private readonly IOrderService _orderService;
-
+        private ViewModel _currentModel;
+        
+        #region ShowEmployeesViewCommand (отображение представления сотрудников)
+        private ICommand _showEmployeesViewCommand;
+        public ICommand ShowEmployeesViewCommand => _showEmployeesViewCommand
+            ??= new LambdaCommand(OnShowEmployeesViewCommanExecuted, CanShowEmployeeCommandExecute);
+        private bool CanShowEmployeeCommandExecute() => true; 
+        private void OnShowEmployeesViewCommanExecuted()
+        {
+            CurrentModel = new EmployeesViewModel(_employeeRepository);
+        }
+        #endregion
         public string Title
         {
             get => _title; set => Set(ref _title, value);            
         }
+        public ViewModel CurrentModel { get => _currentModel; private set => Set(ref _currentModel, value); }
         public MainWindowViewModel(IRepository<Employee> employeeRepository, IOrderService orderService)
         {
             _employeeRepository = employeeRepository;
